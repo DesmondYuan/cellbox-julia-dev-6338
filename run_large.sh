d89231950ba81fda34c087b36226b28cf6b1c323
Bo Yuan <boyuan@g.harvard.edu>
2020-12-15 15:49:54 -0800

    migrate job details to history.md

--- run_large.sh
+++ run_large.sh
@@ -11,15 +11,4 @@ julia="/n/data1/hms/cellbio/sander/bo/julia/installation/julia-1.5.3/bin/julia"
 cd /n/data1/hms/cellbio/sander/bo/julia/cellbox-julia-dev-6338
 ${julia} -p 8 ${1}
 
-# 20-30 min large jobs
-# Task A - Part I - 48 * 20 min
-# sbatch run_large.sh final_train5_o2_TaskA/net5_50.jl
-# Task A - Part II - 48 * 20 min
-# sbatch run_large.sh final_train5_o2_TaskA/net10_20.jl
-# Task A - Part III - 24 * 40 min
-# sbatch run_large.sh final_train5_o2_TaskA/net100.jl
-
-# Task B - Part I - 96 * 10 min
-# sbatch run_large.sh final_train5_o2_TaskB/ts1_40.jl
-# Task B - Part II - 96 * 10 min
-# sbatch run_large.sh final_train5_o2_TaskB/ts5_10.jl
+# 20-30 min large job
